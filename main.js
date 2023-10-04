4be305a5dee05ed87cf4ec032e8dba412df72255
Exxy01 <105254222+Exxy01@users.noreply.github.com>
2023-10-04 19:34:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup(){
+  canvas =  createCanvas(400, 400);
+  canvas.position(500, 150);
+  video = createCapture(VIDEO);
+  video.size(550, 500)
+ posenet = ml5.poseNet(video, modelLoaded)
+ posenet.on("pose", gotPoses)
+}
+
+function draw(){
+  background("#0000FF")
+}
+function modelLoaded(){
+  console.log("Posenet is working, by the way, if you see this have a good day!")
+}
+function gotPoses(results){
+  if(results.length > 0){
+    console.log(results);
+  }
+}
